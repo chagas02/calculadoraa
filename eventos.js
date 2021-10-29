70108c7fffbb398fa7ab96d48330a4da801eb685
chagas02 <francisco_publicidade@yahoo.com.br>
2021-10-29 09:52:01 -0300

    adicionei function subtrair, multiplicar, dividir e igual

--- eventos.js
+++ eventos.js
@@ -1,16 +1,80 @@
-let Visor = document.getElementById('visor');
+const VISOR = document.getElementById('visor');
+let operacao = false;
+let memoriaAuxiliar = 0;
 
 
 function selecionarNumero(numero) {
 //quantidade de caracteres
-if (Visor.innerHTML.length > 9){
+if (VISOR.innerHTML.length > 9){
   return;
 }
-if (Visor.innerHTML == 0) {
-    Visor.innerHTML = numero;
+
+if (numero == 0 && VISOR.innerHTML == 0) {
+  return;
+}
+
+if (VISOR.innerHTML == 0) {
+    VISOR.innerHTML = numero;
       return ;
+  }
+
+  if (operacao ===true && memoriaAuxiliar != 0) {
+    VISOR.innerHTML = numero;
+    operacao = false;
+    VISOR.style.color = '#fff';
+    return;
+  }
+  VISOR.innerHTML += numero;
+
+} //fechando função selecionarNumero
+
+function limpar() {
+  VISOR.innerHTML = 0;
+  memoriaAuxiliar = 0;
+  operacao = false;
+
+VISOR.style.color = '#fff';
+
+}
+
+function somar() {
+  operacao = true;
+  VISOR.style.color = '#888';
+  if (memoriaAuxiliar !== 0) {
+    VISOR.innerHTML = memoriaAuxiliar + parseInt(VISOR.innerHTML);
 
   }
-  Visor.innerHTML += numero;
+ memoriaAuxiliar = parseInt(VISOR.innerHTML);
+}
 
+function subtrair() {
+  operacao = true;
+  VISOR.style.color = '#888';
+  if (memoriaAuxiliar != 0) {
+    VISOR.innerHTML = memoriaAuxiliar - parseInt(VISOR.innerHTML);
+  }
+  memoriaAuxiliar = parseInt(VISOR.innerHTML);
+}
+
+function multiplicar() {
+  operacao = true;
+  VISOR.style.color = '#888';
+  if (memoriaAuxiliar != 0) {
+    VISOR.innerHTML = memoriaAuxiliar * parseInt(VISOR.innerHTML);
+  }
+  memoriaAuxiliar = parseInt(VISOR.innerHTML);
+
+}
+function dividir() {
+  operacao = true;
+  VISOR.style.color = '#888';
+  if (memoriaAuxiliar != 0) {
+    VISOR.innerHTML = memoriaAuxiliar / parseInt(VISOR.innerHTML);
+  }
+  memoriaAuxiliar = parseInt(VISOR.innerHTML);
+}
+
+function igual() {
+  operacao = true;
+  resultado = parseInt(VISOR.innerHTML);
 }
